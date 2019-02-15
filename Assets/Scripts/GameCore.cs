2ad709573dce5c51851cf6945a0cd9ddf5082731
jfields4 <jfields4@harding.edu>
2019-02-14 18:14:44 -0600

    Added game core
    We know this is broken, but we are integrating now

---
+++ Assets/Scripts/GameCore.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameCore : MonoBehaviour
+{
+    Board board;
+
+    bool AIGame;
+    Board::Move LastMove;
+
+    public GameCore(bool AIOption)
+    {
+        AIGame = AIOption;
+        //will need to initialize modules
+    }
+
+    public bool ValidateMove(string move)
+    {
+        return board.ValidateMove(move);
+    }
+
+    public bool ProcessMove(string move)
+    {
+        bool result = false;
+
+        LastMove = Board.ConvertStringToMove(move);
+
+        if (board.MakeMove(move))
+        {
+            result = true;
+        }
+        else
+        {
+            if (!AIGame)
+            {
+                network.SendMove(move);
+            }
+
+            string newMove = GetNewMove();
+
+            if (Board.MakeMove(newMove))
+            {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
+    private string GetNewMove()
+    {
+        Board::Move move;
+
+        if (AIGame)
+        {
+            getAIMove(LastMove);
+        }
+        else
+        {
+            network.GetMove();
+        }
+    }
+}
