65a4add5df76d08522663b91d1bbd662dddd663b
jfields4 <jfields4@harding.edu>
2019-03-31 16:07:52 -0500

    worked on music options

---
+++ Assets/Scripts/UI/MusicVolumeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicVolumeController : MonoBehaviour
+{
+    public GameObject music;
+    public AudioSource audio;
+    public Slider slider;
+
+    public void Awake()
+    {
+        music = GameObject.Find("Music");
+        Debug.Log("Music check: " + music);
+
+        audio = music.GetComponent<AudioSource>();
+        Debug.Log("Audio check: " + audio);
+
+        slider = GetComponent<Slider>();
+        Debug.Log("Slider check: " + slider);
+    }
+
+    public void OnValueChanged()
+    {
+        audio.volume = slider.value;
+        Debug.Log(slider.value);
+    }
+}
