0f708d4e427bce8040ae2b47c2006472d17d57d4
jfields4 <jfields4@harding.edu>
2019-04-12 17:50:03 -0500

    Re-added End screens. Still need to add logic to switch to them

---
+++ Assets/Scripts/UI/EndScreenManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class EndScreenManager : MonoBehaviour
+{
+    public Button mainMenuButton;
+
+    void OnEnable()
+    {
+        mainMenuButton.onClick.AddListener(ReturnToMainMenu);
+    }
+
+    void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
