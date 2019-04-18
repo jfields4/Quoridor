94e79053b8f2a3b0f4428523e4b444cb7c523f4c
jfields4 <jfields4@harding.edu>
2019-04-17 20:38:10 -0500

    Added intro animation

---
+++ Assets/Scripts/UI/IntroController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    public void Start()
+    {
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            EndReached(videoPlayer);
+        }
+    }
+
+    public void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        vp.Stop();
+        SceneManager.LoadScene(1);
+    }
+}
